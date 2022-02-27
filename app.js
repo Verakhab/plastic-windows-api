4870e67041b6a697e1345a5c8c59a89be0cb1b0e
Denis Bakharev <verakhab@MacBook-Pro-Denis.local>
2022-02-27 21:38:18 +0300

    fixed bugs

--- app.js
+++ app.js
@@ -22,15 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/amount', usersRouter);
